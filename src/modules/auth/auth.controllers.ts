6e867c707b24826208caf843eec56086987e22a7
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-03 01:36:35 -0400

    refactor: update registerController to use IUser interface
    - Updated `registerController` to use the `IUser` interface for request body validation.
    - Ensured the `createUser` service function now works with the `IUser` interface for user creation.

--- src/modules/auth/auth.controllers.ts
+++ src/modules/auth/auth.controllers.ts
@@ -1,13 +1,13 @@
 import { Request, Response } from 'express'
 import { createUser } from './auth.services'
-import { IAuth } from './auth.interfaces'
+import { IUser } from './auth.interfaces'
 import handleHttpError from '../../utils/error.handle'
 import { AppError } from '../../utils/errors'
 
-const registerController = async (req: Request<{}, {}, IAuth>, res: Response) => {
+const registerController = async (req: Request<unknown, unknown, IUser>, res: Response) => {
   try {
-    const response = await createUser(req.body)
-    res.status(201).send(response)
+    const registeredUser = await createUser(req.body)
+    res.status(201).send(registeredUser)
   } catch (error) {
     handleHttpError(res, error as AppError)
   }
