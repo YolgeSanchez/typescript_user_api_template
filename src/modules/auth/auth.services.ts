339d0c4391fa414a735d2d76deb9a78836a6455e
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-03 01:24:15 -0400

    refactor: update createUser function to use IUser interface
    - Modified database operation functions to align with the new `IUser` interface.
    - Ensured consistent usage of `IUser` across the code for user creation and related database operations.

--- src/modules/auth/auth.services.ts
+++ src/modules/auth/auth.services.ts
@@ -1,20 +1,20 @@
 import User from './auth.models'
-import { IAuth } from './auth.interfaces'
+import { IUser } from './auth.interfaces'
 import { encrypt } from '../../utils/bcrypt.handle'
 import { AppError } from '../../utils/errors'
 
-const createUser = async (user: IAuth) => {
+const createUser = async (user: IUser) => {
   // verify not existing user with this email
-  const userExists = await getByEmail(user.email)
-  if (userExists) throw new AppError('EMAIL_ALREADY_IN_USE', 409)
+  const isEmailInUse = await getByEmail(user.email)
+  if (isEmailInUse) throw new AppError('EMAIL_ALREADY_IN_USE', 409)
 
   // hashed password
   const password = await encrypt(user.password)
 
   // create the user and save it
   try {
-    const newUser = await insertUser({ ...user, password })
-    return newUser
+    const insertedUser = await insertUser({ ...user, password })
+    return insertedUser
   } catch (error) {
     throw new AppError('SERVER_INTERNAL_ERROR_CREATING_USER', 500)
   }
@@ -22,11 +22,11 @@ const createUser = async (user: IAuth) => {
 
 // repetitive database queries operations
 const getUsers = () => User.find()
-const insertUser = (user: IAuth) => new User(user).save()
+const insertUser = (user: IUser) => new User(user).save()
 const getUser = (id: string) => User.findById(id)
 const deleteUser = (id: string) => User.findByIdAndDelete(id)
 const getByEmail = (email: string) => User.findOne({ email })
-const updateUser = (id: string, updatedUser: IAuth) =>
+const updateUser = (id: string, updatedUser: IUser) =>
   User.findByIdAndUpdate(id, updatedUser, { new: true })
 
 //exports
