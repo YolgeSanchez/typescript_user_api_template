09ed63c24cc56fa63972655fcbb87886d31d9565
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-03 01:21:56 -0400

    refactor: update IUser and IAuth interfaces for user and authentication models
    - Defined `IUser` interface with `name`, `email`, and `password` fields to structure the user model
    - Defined `IAuth` interface with `email` and `password` fields to align with the authentication schema

--- src/modules/auth/auth.interfaces.ts
+++ src/modules/auth/auth.interfaces.ts
@@ -1,6 +1,12 @@
-export interface IAuth {
+export interface IUser {
   name: string
   email: string
   password: string
-  // replace or add any field you want for your model
+  // replace or add any field you want for your user model interface
+}
+
+export interface IAuth {
+  email: string
+  password: string
+  // replace or add any field you want for your authentication interface
 }
