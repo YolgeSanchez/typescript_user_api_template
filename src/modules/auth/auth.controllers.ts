d39150f5fa1c9dff191cdc2f5ad4258578b4172d
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-04 19:29:44 -0400

    refactor: instantiate the new auth service class in controllers

--- src/modules/auth/auth.controllers.ts
+++ src/modules/auth/auth.controllers.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express'
-import { createUser } from './auth.services'
 import { IUser } from './auth.interfaces'
 import handleHttpError from '../../utils/error.handle'
 import { AppError } from '../../utils/errors'
+import AuthService from './auth.services'
 
 const registerController = async (req: Request<unknown, unknown, IUser>, res: Response) => {
   try {
-    const registeredUser = await createUser(req.body)
+    const registeredUser = await AuthService.createUser(req.body)
     res.status(201).send(registeredUser)
   } catch (error) {
     handleHttpError(res, error as AppError)
