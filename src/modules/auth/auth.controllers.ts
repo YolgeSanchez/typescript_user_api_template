62d4a54ef3b33d876bc29c6d2c4e7ca35ac0fe88
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-05 06:09:56 -0400

    refactor: convert to class structure and add login method
    - Refactored `AuthController` into a class structure for consistency with `AuthService` and `AuthRepository`.
    - Added a new `login` method to handle user authentication using `AuthService.authUser`.
    - Updated the `register` method to align with the new class structure.
    - This refactor improves code modularity and maintainability by using instantiable classes.

--- src/modules/auth/auth.controllers.ts
+++ src/modules/auth/auth.controllers.ts
@@ -1,17 +1,30 @@
 import { Request, Response } from 'express'
-import { IUser } from './auth.interfaces'
+import { IAuth, IUser } from './auth.interfaces'
 import handleHttpError from '../../utils/error.handle'
 import { AppError } from '../../utils/errors'
 import AuthService from './auth.services'
 
-const registerController = async (req: Request<unknown, unknown, IUser>, res: Response) => {
-  try {
-    const registeredUser = await AuthService.createUser(req.body)
-    res.status(201).send(registeredUser)
-  } catch (error) {
-    handleHttpError(res, error as AppError)
+class AuthController {
+  // register user
+  register = async (req: Request<unknown, unknown, IUser>, res: Response) => {
+    try {
+      const registeredUser = await AuthService.createUser(req.body)
+      res.status(201).send(registeredUser)
+    } catch (error) {
+      handleHttpError(res, error as AppError)
+    }
+  }
+
+  // authenticate user
+  login = async (req: Request<unknown, unknown, IAuth>, res: Response) => {
+    try {
+      const authenticatedUser = await AuthService.authUser(req.body)
+      res.status(200).send(authenticatedUser)
+    } catch (error) {
+      handleHttpError(res, error as AppError)
+    }
   }
 }
 
 //exports
-export { registerController }
+export default new AuthController()
