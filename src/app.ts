a9bcbd2f646e6fb9f445f39e3250755eeac2e701
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-06 16:11:25 -0400

    feat(app): add cookie-parser middleware for handling cookies

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import 'dotenv/config'
 import express from 'express'
 import cors from 'cors'
 import authRoutes from './modules/auth/auth.routes'
+import cookieParser from 'cookie-parser'
 
 const app = express()
 
@@ -9,6 +10,7 @@ const app = express()
 // apply the cors setting you want
 app.use(cors())
 app.use(express.json())
+app.use(cookieParser())
 
 // [routes]
 app.use('/api/auth', authRoutes)
