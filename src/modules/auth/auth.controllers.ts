d70f8afd618baa4532ae133ec6d3bbdceb429845
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-06 16:17:12 -0400

    feat(auth): add JWT token generation and set cookies on register and login

--- src/modules/auth/auth.controllers.ts
+++ src/modules/auth/auth.controllers.ts
@@ -1,14 +1,16 @@
 import { Request, Response } from 'express'
-import { IAuth, IUser } from './auth.interfaces'
+import { IAuth, IPublicUserData, IUser } from './auth.interfaces'
 import handleHttpError from '../../utils/error.handle'
 import { AppError } from '../../utils/errors'
 import AuthService from './auth.services'
+import jwtUtils from '../../utils/jwt.handle'
 
 class AuthController {
   // register user
   register = async (req: Request<unknown, unknown, IUser>, res: Response) => {
     try {
       const registeredUser = await AuthService.createUser(req.body)
+      res.cookie('token', jwtUtils.generateToken(registeredUser))
       res.status(201).send(registeredUser)
     } catch (error) {
       handleHttpError(res, error as AppError)
@@ -19,6 +21,7 @@ class AuthController {
   login = async (req: Request<unknown, unknown, IAuth>, res: Response) => {
     try {
       const authenticatedUser = await AuthService.authUser(req.body)
+      res.cookie('token', jwtUtils.generateToken(authenticatedUser))
       res.status(200).send(authenticatedUser)
     } catch (error) {
       handleHttpError(res, error as AppError)
