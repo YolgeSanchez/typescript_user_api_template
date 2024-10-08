438699f5b4007bf8e0d3fa1da10b2e953d29c2dd
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-08 14:57:28 -0400

    feat(auth): add logout route to authentication router

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -2,8 +2,6 @@ import { Router } from 'express'
 import { validate } from '../../middlewares/dataValidation'
 import { authSchema, registerSchema } from './auth.schemas'
 import AuthController from './auth.controllers'
-import { auth } from '../../middlewares/auth.middleware'
-import { RequestExt } from '../../types/express'
 
 const router = Router()
 
@@ -11,12 +9,11 @@ const router = Router()
  * [authentication routes]
  * http://localhost:3001/api/auth/register [POST]
  * http://localhost:3001/api/auth/login [POST]
+ * http://localhost:3001/api/auth/logout [GET]
  */
 router.post('/register', validate(registerSchema), AuthController.register)
 router.post('/login', validate(authSchema), AuthController.login)
-router.use(auth())
-router.get('/protected', (req: RequestExt, res) => {
-  res.send({ message: 'solo un usuario autenticado lo puede ver', user: req.user })
-})
+router.get('/logout', AuthController.logout)
 
+// export the routes
 export default router
