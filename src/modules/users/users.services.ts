1fcdef137f324a561278b2f1d7a22668d3756ddd
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-09 01:12:49 -0400

    refactor(users): ensure consistent return type in update and delete user services

--- src/modules/users/users.services.ts
+++ src/modules/users/users.services.ts
@@ -22,6 +22,7 @@ class UserServices {
   getUsers = async (): Promise<IPublicUserData[]> => {
     const users = await UserRepository.getUsers()
     if (!users) throw new AppError('USERS_NOT_FOUND', 404)
+    // create a new array with the user data interface structure
     const usersData = users.map((user) => {
       // change this if you add more files that you want to return
       const userData = {
@@ -34,25 +35,35 @@ class UserServices {
     return usersData
   }
 
-  updateUser = async (id: string, updatedUser: IUser) => {
+  updateUser = async (id: string, updatedUser: IUser): Promise<IPublicUserData> => {
     try {
       const user = await UserRepository.updateUser(id, updatedUser)
       if (!user) {
         throw new AppError('USER_NOT_FOUND', 404)
       }
-      return user
+      const publicUserData = {
+        id: user._id.toHexString(),
+        name: user.name,
+        email: user.email,
+      }
+      return publicUserData
     } catch (error) {
       throw new AppError('USER_NOT_FOUND', 404)
     }
   }
 
-  deleteUser = async (id: string) => {
+  deleteUser = async (id: string): Promise<IPublicUserData> => {
     try {
       const user = await UserRepository.deleteUser(id)
       if (!user) {
         throw new AppError('USER_NOT_FOUND', 404)
       }
-      return user
+      const publicUserData = {
+        id: user._id.toHexString(),
+        name: user.name,
+        email: user.email,
+      }
+      return publicUserData
     } catch (error) {
       throw new AppError('USER_NOT_FOUND', 404)
     }
