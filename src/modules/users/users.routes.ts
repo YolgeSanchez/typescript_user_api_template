34031020e42cb9c2e33457afb9cd88edcbc84b4c
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-09 19:07:36 -0400

    feat(users): update user routes with role-based access and new user creation

--- src/modules/users/users.routes.ts
+++ src/modules/users/users.routes.ts
@@ -2,20 +2,23 @@ import { Router } from 'express'
 import { auth } from '../../middlewares/auth.middleware'
 import UsersControllers from './users.controllers'
 import { validate } from '../../middlewares/dataValidation'
-import { registerSchema } from '../auth/auth.schemas'
+import { registerSchema } from './users.schemas'
 
 const router = Router()
 
 /**
  * [users routes]
- * http://localhost:3001/api/users [GET]
- * http://localhost:3001/api/users/:id [GET]
+ * http://localhost:3001/api/users [GET] [admin & user]
+ * http://localhost:3001/api/users/:id [GET] [admin & user]
+ * http://localhost:3001/api/users/:id [PUT] [admin only]
+ * http://localhost:3001/api/users/:id [DELETE] [admin only]
+ * http://localhost:3001/api/users [POST] [admin only]
  */
-router.use(auth())
-router.get('/', UsersControllers.getUsers)
-router.get('/:id', UsersControllers.getUser)
-router.put('/:id', validate(registerSchema), UsersControllers.updateUser)
-router.delete('/:id', UsersControllers.deleteUser)
+router.get('/', auth(['admin', 'user']), UsersControllers.getUsers)
+router.get('/:id', auth(['admin', 'user']), UsersControllers.getUser)
+router.put('/:id', auth(['admin']), validate(registerSchema), UsersControllers.updateUser)
+router.delete('/:id', auth(['admin']), UsersControllers.deleteUser)
+router.post('/', auth(['admin']), validate(registerSchema), UsersControllers.createUser)
 
 // export the routes
 export default router
