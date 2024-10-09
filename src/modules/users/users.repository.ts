6bea818d010da3da4568957d27e396fb1a2943a1
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-09 19:08:03 -0400

    feat(users): add user insertion method in repository

--- src/modules/users/users.repository.ts
+++ src/modules/users/users.repository.ts
@@ -2,6 +2,7 @@ import { IUser } from './users.interfaces'
 import User from './users.models'
 
 class UserRepository {
+  insertUser = (user: IUser) => new User(user).save()
   getUsers = () => User.find()
   getUser = (id: string) => User.findById(id)
   deleteUser = (id: string) => User.findByIdAndDelete(id)
