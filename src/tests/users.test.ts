6c6bb9bd1aa072b4ccc8faab4bf887b70c5c8f49
YolgeSanchez <imjorgesanchez@gmail.com>
2024-10-11 19:34:37 -0400

    test: add API tests for user creation, login, and CRUD operations
    - Implemented test suite for User API with Supertest.
    - Added tests for user registration, login, and role-based authorization.
    - Added tests for user creation, retrieval, update, and deletion.
    - Introduced token-based authentication checks for all user routes.
    - Handled cases for unauthorized access (401), permission denied (403), resource not found (404), and email conflict (409).
    - Configured beforeAll and afterAll hooks for MongoDB connection and cleanup.
    - Managed unhandled promise rejections to improve test robustness.

--- src/tests/users.test.ts
+++ src/tests/users.test.ts
@@ -2,11 +2,22 @@ import request from 'supertest'
 import app from '../app'
 import mongoose from 'mongoose'
 import 'dotenv/config'
+import initAdminUser from '../utils/adminUser.handle'
+
+let name: string | undefined
+let email: string | undefined
+let password: string | undefined
+let role: string | undefined
 
 beforeAll(async () => {
   await mongoose.connect(process.env.DB_URI || 'your-mongodb-uri')
+  await mongoose.connection.dropCollection('users')
+  const user = await initAdminUser()
+  name = user?.name
+  email = user?.email
+  password = user?.password
+  role = user?.role
   //drop the database before initializing the test suite
-  await mongoose.connection.dropDatabase()
 })
 
 afterAll(async () => {
@@ -27,20 +38,53 @@ describe('User API', () => {
   // create users for testing
   let id: string
   let token: string
-  it('should create a new user and return status 201 when valid registration data is provided', async () => {
-    const response = await request(app).post('/api/auth/register').send({
+  let notAuthorizedToken: string
+  it('should return 200 when logging in with admin credentials', async () => {
+    const response = await request(app).post('/api/auth/login').send({
+      email,
+      password,
+    })
+    expect(response.status).toBe(200)
+    expect(response.body).toEqual({
+      id: expect.any(String),
+      name,
+      email,
+      role,
+    })
+    token = response.headers['set-cookie'][0].split(';')[0]
+  })
+
+  it('should return status 201 when creating a new user', async () => {
+    const response = await request(app).post('/api/users').set('Cookie', token).send({
       name: 'jane doe',
       email: 'janedoe@example.com',
       password: 'example',
+      role: 'user',
     })
     expect(response.status).toBe(201)
     expect(response.body).toEqual({
       id: expect.any(String),
       name: 'jane doe',
       email: 'janedoe@example.com',
+      role: 'user',
+    })
+    const login = await request(app).post('/api/auth/login').send({
+      email: 'janedoe@example.com',
+      password: 'example',
     })
     id = response.body.id
-    token = response.headers['set-cookie'][0].split(';')[0]
+    notAuthorizedToken = login.headers['set-cookie'][0].split(';')[0]
+  })
+
+  it('should return status 409 when attempting to create a user with an existing email', async () => {
+    const response = await request(app).post('/api/users').set('Cookie', token).send({
+      name: 'jane doe',
+      email: 'janedoe@example.com',
+      password: 'example',
+      role: 'user',
+    })
+    expect(response.status).toBe(409)
+    expect(response.body).toEqual({ message: 'EMAIL_ALREADY_IN_USE' })
   })
 
   it('should return status 200 when retrieving all users with token', async () => {
@@ -49,13 +93,15 @@ describe('User API', () => {
     expect(response.body).toEqual([
       {
         id: expect.any(String),
-        name: 'john doe',
-        email: 'johndoe@example.com',
+        name,
+        email,
+        role,
       },
       {
         id: expect.any(String),
         name: 'jane doe',
         email: 'janedoe@example.com',
+        role: 'user',
       },
     ])
   })
@@ -67,6 +113,7 @@ describe('User API', () => {
       id: expect.any(String),
       name: 'jane doe',
       email: 'janedoe@example.com',
+      role: 'user',
     })
   })
 
@@ -78,18 +125,48 @@ describe('User API', () => {
     expect(response.body).toEqual({ message: 'USER_NOT_FOUND' })
   })
 
+  it('should return status 403 when updating a user with a not authorized token', async () => {
+    const response = await request(app)
+      .put(`/api/users/${id}`)
+      .set('Cookie', notAuthorizedToken)
+      .send({
+        name: 'new jane doe',
+        email: 'newjanedoe@example.com',
+        password: 'example',
+        role: 'user',
+      })
+    expect(response.status).toBe(403)
+    expect(response.body).toEqual({ message: 'YOU_DONT_HAVE_THE_PERMISSION' })
+  })
+
   it('should return status 200 when updating a user with token', async () => {
     const response = await request(app).put(`/api/users/${id}`).set('Cookie', token).send({
       name: 'new jane doe',
       email: 'newjanedoe@example.com',
       password: 'example',
+      role: 'admin',
     })
     expect(response.status).toBe(200)
     expect(response.body).toEqual({
       id,
       name: 'new jane doe',
       email: 'newjanedoe@example.com',
+      role: 'admin',
     })
+
+    // return this user role to 'user'
+    await request(app).put(`/api/users/${id}`).set('Cookie', token).send({
+      name: 'new jane doe',
+      email: 'newjanedoe@example.com',
+      password: 'example',
+      role: 'user',
+    })
+  })
+
+  it('should return status 403 when deleting a user with a not authorized token', async () => {
+    const response = await request(app).delete(`/api/users/${id}`).set('Cookie', notAuthorizedToken)
+    expect(response.status).toBe(403)
+    expect(response.body).toEqual({ message: 'YOU_DONT_HAVE_THE_PERMISSION' })
   })
 
   it('should return status 200 when deleting a user with token', async () => {
@@ -99,6 +176,9 @@ describe('User API', () => {
       id,
       name: 'new jane doe',
       email: 'newjanedoe@example.com',
+      role: 'user',
     })
+
+    await request(app).get('/api/auth/logout')
   })
 })
